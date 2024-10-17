5914def84826a4ac7314520a5a73dd6465070aa6
madhav <eragon.cube@outlook.com>
2024-10-17 20:30:10 +0530

    Timeline Prototype

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = "NextGenAI Lab | NSUT";
+export const SITE_DESCRIPTION =
+  "NextGenAI Lab is a research group at the Netaji Subhas University of Technology, New Delhi, focusing on the development of next-generation AI technologies.";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
