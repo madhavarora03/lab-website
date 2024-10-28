43a0ebfe183c7fce6169e7db3a5fad6ef80b0364
Madhav Arora <eragon.cube@outlook.com>
2024-10-28 20:28:56 +0530

    Home Page v1

--- src/config.ts
+++ src/config.ts
@@ -5,4 +5,3 @@ export const SITE_TITLE = "NextGenAI Lab | NSUT";
 export const SITE_DESCRIPTION =
   "NextGenAI Lab is a research group at the Netaji Subhas University of Technology, New Delhi, focusing on the development of next-generation AI technologies.";
 export const GENERATE_SLUG_FROM_TITLE = true;
-export const TRANSITION_API = true;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
+import flowbite from "flowbite/plugin";
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   daisyui: {
     themes: ["light"],
   },
@@ -9,5 +13,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [flowbite, daisyui],
 };
